f8389e8064147333442452251b091e2642f14771
Thiago Souza <30611794+Thiagothqs@users.noreply.github.com>
2025-01-30 23:24:35 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto do TabNews do curso.dev do Filipe Deschamps
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Família e amizade é tudoo!!</h1>
+function Home() {
+  return <h1>Família e amizade é tudoo!!</h1>;
 }
 
 export default Home;
